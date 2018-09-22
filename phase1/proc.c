9df097af58587aff5b2490f18cfa8725d5b541fe
GeekBoyChan <37280506+GeekBoyChan@users.noreply.github.com>
2018-09-21 17:35:14 -0700

    Update proc.c

--- phase1/proc.c
+++ phase1/proc.c
@@ -11,15 +11,14 @@ void InitProc(void) {
    unsigned short *p = 0xb8000; // upper-left corner of display
 
    while(1) {
-      cons_putchar('.'+VGA_MASK); //show the dot
-      *p = '.' + VGA_MASK;
+      p = '.' + VGA_MASK;  //show the dot
       //wait for half of LOOP: loop on asm("inb $0x80");
       for(i=0, i<LOOP/2, i++)
       {
         asm("inb $0x80");
       }
 
-      *p = ' ' + VGA_MASK; //erase above writing
+      p = ' ' + VGA_MASK; //erase above writing
       //wait for half of LOOP: loop on asm("inb $0x80");
       for(i=0; i<LOOP/2;i++)
       {
@@ -34,17 +33,22 @@ void UserProc(void) {
 
    while(1) {
       int offset = cur_pid *160; //point p to (0xb8000 + offset according to its PID)
-      *p = (cur_pid/10+'0') + VGA_MASK;//show 1st digit of its PID
+      p = ((cur_pid/10) +'0') + VGA_MASK;//show 1st digit of its PID
       p++; //move p to next column
-      *p = (cur_pid%10 + '0') + VGA_MASK; // show 2nd digit of its PID
+      p = ((cur_pid%10) + '0') + VGA_MASK; // show 2nd digit of its PID
       //wait for half of LOOP: loop on asm("inb $0x80");
       for(i=0;i<LOOP/2;i++)
       {
         asm("inb $0x80");
       }
       //erase above writing
-      *p = ' ' + VGA_MASK;
+      p = ' ' + VGA_MASK; //Clears the 2nd digit
+      p--;                //Move back a column
+      p = ' ' +VGA_MASK;  //Clears the 1st digit
       //wait for half of LOOP: loop on asm("inb $0x80");
-      for(i=0;i<LOOP/2;i++) asm("inb $0x80");
+      for(i=0;i<LOOP/2;i++)
+      {
+         asm("inb $0x80");
+      }
    }
 }
