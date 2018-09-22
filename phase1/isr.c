44377fd06cc549590c370bd4ba2c4e438cb00726
aencinas <aencinas94@gmail.com>
2018-09-21 18:54:22 -0700

    Update isr.c

--- phase1/isr.c
+++ phase1/isr.c
@@ -9,40 +9,46 @@
 
 // to create a process: alloc PID, PCB, and process stack
 // build trapframe, initialize PCB, record PID to ready_q
-void NewProcISR(func_p_t p) {  // arg: where process code starts
-   int pid;
 
-   if( avail_q is empty ) {    // may occur if too many been created
-      cons_printf("Kernel panic: no more process!\n");
-      ...                      // cannot continue, alternative: breakpoint();
-   }
+void NewProcISR(func_p_t p) 
+{  // arg: where process code starts
+	int pid;
 
-   ...                                       // alloc PID (1st is 0)
-   ...                                       // clear PCB
-   ...                                       // clear stack
-   ...                                       // change process state
-
-   ...                                       // queue it
-
+	if(avail_q.size == 0 )
+	{    // may occur if too many been created
+		cons_printf("Kernel panic: no more process!\n");
+        	return;        // cannot continue, alternative: breakpoint();
+   	}
+	
+   	pid = DeQ(&avail_q);                 // alloc PID (1st is 0)
+   	Bzero((char *) &pcb[pid], sizeof(pcb_t));       // clear PCB
+   	Bzero((char *) &stack[pid] [0], STACK_SIZE);  // clear stack
+   	pcb[pid].state = READY;              // change process state
+	   		
+	EnQ(pid, &ready_pid_q);              // queue it
+	
 // point TF_p to stack & fill it out
-   pcb[...].TF_p =                               
-   pcb[...].TF_p--;
-   pcb[...].TF_p->efl = EF_DEFAULT_VALUE|EF_INTR; // enables intr
-   pcb[...].TF_p->cs = get_cs();                  // duplicate from CPU
-   pcb[...].TF_p->eip =                           // set to code
+
+   pcb[pid].TF_p = (TF_t *) &stack[pid][STACK_SIZE - sizeof(TF_t)];                              
+   pcb[pid].TF_p--;
+   pcb[pid].TF_p->efl = EF_DEFAULT_VALUE|EF_INTR; // enables intr
+   pcb[pid].TF_p->cs = get_cs();                  // duplicate from CPU
+   pcb[pid].TF_p->eip = p;                          // set to code
 }
 
 // count run time and switch if hitting time limit
+
+
 void TimerISR(void) {
-   outportb(...                              // notify PIC getting done
+   	outportb(PIC_CONTROL, DONE);              // notify PIC getting done
 
-   ...                                       // count up time
-   ...                                       // count up life
+   	pcb[cur_pid].time++;                      // count up time
+   	pcb[cur_pid].life++;                      // count up life
 
-   if(...                          ) {       // if runs long enough
-      ...                                    // move it back to ready_q
-      ...                                    // change its state
-      ...                                    // now no running proc
-   }
+   	if(pcb[cur_pid].runtime == TIME_MAX)
+	{
+		EnQ(cur_pid, &ready_pid_q);  //append current pid to ready queue
+		cur_pid = -1;                //reset current pid to -1
+		pcb[cur_pid].state = READY;  //change state to ready
+	}
 }
-
