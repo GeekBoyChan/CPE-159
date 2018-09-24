53665ef63cc9a18bbcb7f541fd393899da975dab
aencinas <aencinas94@gmail.com>
2018-09-23 17:01:28 -0700

    Update include.h

--- phase1/include.h
+++ phase1/include.h
@@ -2,6 +2,7 @@
 // SPEDE library headers, ~spede2/Target-i386/Common/include/spede
 // Team Name: LIGMAOS
 // Members: Andrew Encinas, Chandler Ocapan, Alex Paraiso
+// Phase 1
 
 #ifndef __INCLUDE__
 #define __INCLUDE__
