9bd77241d18ee8d789edce3649998d1e7f231b3f
tinytaiga <37280888+tinytaiga@users.noreply.github.com>
2018-10-06 17:52:35 -0700

    Update isr.c

--- phase3/isr.c
+++ phase3/isr.c
@@ -18,7 +18,7 @@ void NewProcISR(func_p_t p)
 {  // arg: where process code starts
 	int pid;
 
-	if(avail_q.size == 0 )
+	if(QisEmpty(p))
 	{    // may occur if too many been created
 		cons_printf("Kernel panic: no more process!\n");
         	return;        // cannot continue, alternative: breakpoint();
