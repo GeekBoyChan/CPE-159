43a2d27507c07e3582fe1d0e1eee48dc50ed7343
GeekBoyChan <37280506+GeekBoyChan@users.noreply.github.com>
2018-10-06 21:36:53 -0700

    Comments gone

--- phase3/proc.c
+++ phase3/proc.c
@@ -4,9 +4,9 @@
 
 // Team Name: LIGMAOS
 // Members: Andrew Encinas, Chandler Ocapan, Alex Paraiso
-// Phase 2
+// Phase 3
 
-#include "constants.h" // include only these 2 files
+#include "constants.h"        	// include only these 2 files
 #include "syscalls.h"
 #include "data.h"
 
@@ -15,11 +15,11 @@ void InitProc(void) {
    car_sem = SemInit(3);
 
    while(1) {
-      SetVideo(1, 1);         // pos video
+      SetVideo(1, 1);         	// pos video
       Write(STDOUT, ".");
       for(i=0; i<LOOP/2; i++)asm("inb $0x80"); // busy CPU
 
-      SetVideo(1, 1);         // pos video
+      SetVideo(1, 1);         	// pos video
       Write(STDOUT, " ");
       for(i=0; i<LOOP/2; i++)asm("inb $0x80"); // busy CPU
    }
@@ -30,24 +30,24 @@ void UserProc(void)
    int my_pid;
    char str[3];
 
-    my_pid = GetPid(); //get my PID and make a string from it (null-delimited)
-    str[0] = '0' + my_pid/10; //print the first digit of mypid
-    str[1] = '0' + my_pid%10; //print the second digit of mypid
-    str[2] = '\0'; //null delimited
+    my_pid = GetPid();           //get my PID and make a string from it (null-delimited)
+    str[0] = '0' + my_pid/10;    //print the first digit of mypid
+    str[1] = '0' + my_pid%10;    //print the second digit of mypid
+    str[2] = '\0';               //null delimited
 
-   SetVideo(my_pid+1,1); //set video cursor to beginning of my row
+   SetVideo(my_pid+1,1);         //set video cursor to beginning of my row
    Write(STDOUT, "write out that extra long msg to test line wrapped and erasure YEEEEEEEEEEEEEEEEEEEEE HAAAAAAAAAAAAAAAAAAAA"); //write out that extra long msg to test line wrapped and erasure
-   Sleep(2); //sleep for 2 seconds
+   Sleep(2);                     //sleep for 2 seconds
 
    while(1) 
    {
-      SetVideo(my_pid+1,1);//call service to set video cursor to beginning of my row
+      SetVideo(my_pid+1,1);   	//call service to set video cursor to beginning of my row
       Write(STDOUT, str); //call service to write out my PID str
       Sleep(2); //call service to sleep for 2 seconds
 
-      SetVideo(my_pid+1,1); //call service to set video cursor to beginning of my row
-      Write(STDOUT, "--"); //call service to erase my PID str (with "--")
-      Sleep(2); //call service to sleep for 2 seconds
+      SetVideo(my_pid+1,1); 	//call service to set video cursor to beginning of my row
+      Write(STDOUT, "--"); 	//call service to erase my PID str (with "--")
+      Sleep(2); 		//call service to sleep for 2 seconds
    }
 }
 
@@ -55,20 +55,20 @@ void CarProc(void)
 {
 	int my_pid = GetPid();
 	char str[3];
-    	str[0] = '0' + my_pid/10; //print the first digit of mypid
-    	str[1] = '0' + my_pid%10; //print the second digit of mypid
-    	str[2] = '\0'; //null delimited
+    	str[0] = '0' + my_pid/10; 	//print the first digit of mypid
+    	str[1] = '0' + my_pid%10; 	//print the second digit of mypid
+    	str[2] = '\0'; 			//null delimited
 
 	while(1)
 	{
 		SetVideo(my_pid+1,1);
-		Write(STDOUT, str); //call service to write out my PID str
+		Write(STDOUT, str); 	//call service to write out my PID str
 		SetVideo(my_pid+1,10);
 		Write(STDOUT,"I'm off              ");
 		Sleep(2);
 		
 		SemWait(car_sem);
-		SetVideo(my_pid+1,1); //call service to set video cursor to beginning of my row
+		SetVideo(my_pid+1,1); 	//call service to set video cursor to beginning of my row
     		Write(STDOUT, "--");
 		SetVideo(my_pid+1,10);
 		Write(STDOUT, "I'm on the bridge!");
