7bbb4003c228354c9a91157ca57e6f23eca21791
GeekBoyChan <37280506+GeekBoyChan@users.noreply.github.com>
2018-10-20 18:44:41 -0700

    Update isr.c

--- phase5/isr.c
+++ phase5/isr.c
@@ -287,7 +287,6 @@ void TermTxISR(int index)
 void TermRxISR(int interface_num)
 {
 	int pid;
-	char *buff = (char *) pcb[cur_pid].TF_p->ecx;
 	//1. read the character in from the 'io' of the terminal interface
 	char in = inportb(term_if[interface_num].io);
 	//2. if the character is NOT '\n' or '\r' (not Enter or Return):
