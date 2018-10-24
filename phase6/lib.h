2c4e79d01cd189d66c236264e06d356043db3377
Andrew D Encinas <encinasa@athena.ecs.csus.edu>
2018-10-24 16:20:26 -0700

    Initial upload

---
+++ phase6/lib.h
@@ -0,0 +1,15 @@
+// lib.h, 159
+// Team Name: LIGMAOS
+// Members: Andrew Encinas, Chandler Ocapan, Alex Paraiso
+// Phase 5
+
+#ifndef __LIB__
+#define __LIB__
+
+void Bzero(char *p, int bytes);
+int QisEmpty(q_t *p);
+int QisFUll(q_t *p);
+int DeQ(q_t *p);
+void EnQ(int to_add, q_t *p);
+
+#endif
---
+++ phase6/syscalls.c
@@ -0,0 +1,94 @@
+// syscalls.c
+// calls to OS services
+
+// Team name: LIGMAOS
+// Members: Andrew Encinas, Chandler Ocapan, Alex Paraiso
+// Phase 5
+
+#include "constants.h"
+
+void Sleep(int sec) {         // # of seconds
+   asm("movl %0, %%eax;       // service #162 (SLEEP)
+        movl %1, %%ebx;       // sleep period in ticks
+        int $128"
+       :
+       : "g" (SLEEP), "g" (sec)
+       : "eax", "ebx"         // used registers
+       );
+}
+
+int GetPid(void) {
+   int pid;
+
+   asm("movl %1, %%eax;       // service #20 (GETPID)
+        int $128;             // interrupt!
+        movl %%ebx, %0"       // after, copy eax to variable 'pid'
+       : "=g" (pid)           // output
+       : "g" (GETPID)         // input
+       : "eax", "ebx"         // used registers
+    );
+
+   return pid;
+}
+
+void SetVideo(int row, int col) {
+   asm("movl %0, %%eax;
+        movl %1, %%ebx;
+        movl %2, %%ecx;
+        int $128"
+        :
+        : "g"(SETVIDEO), "g"(row), "g"(col)
+        : "eax", "ebx", "ecx");
+}
+
+void Write(int device, char *str) {
+   asm("movl %0, %%eax;
+        movl %1, %%ebx;
+        movl %2, %%ecx;
+        int $128"
+        :
+        : "g"(WRITE), "g"(device), "g"((int)str)
+        : "eax", "ebx", "ecx");
+}
+
+void Read(int device, char *buff) {
+   asm("movl %0, %%eax;
+        movl %1, %%ebx;
+        movl %2, %%ecx;
+        int $128"
+        :
+        : "g"(READ), "g"(device), "g"((int)buff)
+        : "eax", "ebx", "ecx");
+}
+
+int SemInit(int passes) {
+   int sem_id;
+   asm("movl %1, %%eax;             //serive#
+        movl %2, %%ebx;             // passes
+        int $128;
+        movl %%ecx, %0"             //sem_od
+        : "=g" (sem_id)             //output
+        : "g"(SEMINIT), "g"(passes)
+        : "eax", "ebx", "ecx");     //used registers
+   return sem_id;
+}
+
+void SemWait(int sem_id) {
+   asm("movl %0, %%eax;
+        movl %1, %%ebx;
+        int $128"
+        :
+        : "g"(SEMWAIT), "g"(sem_id)
+        : "eax", "ebx");
+}
+
+void SemPost(int sem_id) {
+   asm("movl %0, %%eax;
+        movl %1, %%ebx;
+        int $128"
+        :
+        : "g"(SEMPOST), "g"(sem_id)
+        : "eax", "ebx");
+}
+   
+
