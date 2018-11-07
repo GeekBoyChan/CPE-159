e1af36da8b2c2777226483f717c43b701cb2a33c
GeekBoyChan <37280506+GeekBoyChan@users.noreply.github.com>
2018-11-06 19:06:09 -0800

    Update proc.c

--- phase7/proc.c
+++ phase7/proc.c
@@ -153,7 +153,7 @@ void TermProc(void)
     	str[0] = '0' + my_pid/10; 	//print the first digit of mypid
     	str[1] = '0' + my_pid%10; 	//print the second digit of mypid
 	str[2] = ':';
-    	str[3] = '\0'
+    	str[3] = '\0';
 	
 	device = my_pid % 2; 		// if 0 TERM0, if 1 TERM1
 	if (device == 0)
