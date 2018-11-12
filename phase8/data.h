c68e02ee4953360ada88091f6fe2d551128fdd51
aencinas <aencinas94@gmail.com>
2018-11-11 17:19:14 -0800

    Update to include new kernel data

--- phase8/data.h
+++ phase8/data.h
@@ -21,4 +21,7 @@ extern sem_t sem[SEM_MAX];                // kernel has these semaphores
 extern q_t sem_q;                         // semaphore ID's are initially queued here
 extern int car_sem;                       // to hold a semaphore ID for testing
 extern term_if_t term_if[TERM_MAX];       // Terminal interface
+extern char *rx_p;                        // point to inside the buffer
+extern q_t rx_wait_q;                     // for PID waiting for term KB input
+extern q_t wait_q;                        // to Enq parents calling wait and got blocked
 #endif                                    // endif of ifndef
