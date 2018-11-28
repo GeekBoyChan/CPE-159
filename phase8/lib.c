2563e36bc7a3e6f0be48aaaeff54700610302c72
tinytaiga <37280888+tinytaiga@users.noreply.github.com>
2018-11-27 20:09:34 -0800

    Update lib.c

--- phase8/lib.c
+++ phase8/lib.c
@@ -72,13 +72,23 @@ void EnQ(int to_add, q_t *p)
 int StrCmp(char *s1, char *s2)
 {
    //printf("We're inside StrCmp \n\r");
-   int i;
+   int i = 0;
+   /*
    for(i = 0; s1[i] && s2[i]; i++)
    {
-      if(s1[i] != s2[i])
+      if(s1[i] != s2[i] || s1[i] == '\0' || s2[i] == '\0')
       {
          return 0;
       }
+   }
+   return 1;
+   */
+   
+   while(s1[i] != '\0')
+   {
+      if(s1[i] != s2[i])
+         return 0;
+      i++;
    }
    return 1;
 }
